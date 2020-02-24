5c08a7c8689af96f9c73524a99cf3469361349b1
xcore33 <mauler1987@gmail.com>
2020-02-24 23:15:25 +0300

    feat: add solution reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    return String(Math.abs(n)).split('').reverse().join('');
+    //Метод Math.abs() — дает абсолютное значение аргумента, простыми словами — мы получаем модуль числа. Аргумент может быть int, float, long, double, short, byte.
 }
